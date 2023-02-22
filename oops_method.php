eac53935d3f147183a0b4357161709391fc6adb5
Ashif <ashif.mohammad@bestofmine.com>
2023-02-22 18:49:02 +0530

    display functionality

--- oops_method.php
+++ oops_method.php
@@ -33,7 +33,7 @@ public function check_existdata($title,$img,$date){
     // var_dump($result->num_rows);
 
   // while($row = $result->fetch_assoc()) 
-  // {
+  //
 
   // }
   //  $result=$this->conn->query($sql);
@@ -52,8 +52,30 @@ public function check_existdata($title,$img,$date){
     
      
 }
+/*public function  displayRecords(){
+ $sql="select * from image_title ";
+ //$sql="select * from image_title where title=? or image_url=? or post_date=? ";
+ $statement = $this->conn->prepare($sql);
+ $statement->bind_param('sss'$statement);
+ $statement->execute();
+ $result = $statement->get_result();
+ if($result->num_rows)
+ {
+  return true;	
+ }
+
+
+}*/
+public function displayRecords(){
+ $sql="select * from image_title";
+  $result=$this->conn->query($sql);
+  if($result->num_rows>0){
+    return $result;
+
 }
-//sdffgfhh
+}
+}
+
 use Gt\Dom\HTMLDocument;
 require './vendor/autoload.php';
 $obj=new Scrawl();
@@ -63,37 +85,23 @@ $html_codes = file_get_contents($url);
 //echo $html_codes;
 $document = new HTMLDocument($html_codes);
 $news_cards = $document->querySelectorAll('.card');
-//echo"<pre>";
-//print_r($news_cards);
 foreach($news_cards as $card)
 {
-   //$web_url=$obj->conn->real_escape_string($card->querySelector('.card-title')->href);
-   $title = $obj->conn->real_escape_string($card->querySelector('.card-title')->innerText);
-   $img = $obj->conn->real_escape_string($card->querySelector('.img-fluid')->src);
+    $title = $obj->conn->real_escape_string($card->querySelector('.card-title')->innerText);
+     $img = $obj->conn->real_escape_string($card->querySelector('.img-fluid')->src);
 
-   echo  $time = $card->querySelector('.time-stamp')->innerText;
-     //echo trim($time);
-    //echo $oneHourAgo = date('Y-m-d H:i:s', strtotime($time));
-    // exit;
-
-
-
-
-  // $date= date('Y-m-d H:i:s', strtotime($time));
+     $time = $card->querySelector('.time-stamp')->innerText;
     $date= $obj->conn->real_escape_string(date('Y-m-d H:i:s', strtotime($time)));
-  //$date =$obj->conn->real_escape_string($oneHourAgo);
   $fetch_record=$obj->check_existdata($title,$img,$date);
-  //print_r($fetch_record);
   if($fetch_record){
 
    
- //echo "inserted record  data";
+ 
  }else{
 
-    //echo "not inserted record ";
+   
 
  }
- 
 }
 
 ?>
